ca4e4604436f2a032137f6a9756a7f4a4d61ed3d
mvn-phucnguyen-hn <phuc.nguyen@monstar-lab.com>
2023-12-18 10:45:05 +0700

    Base source commit

---
+++ src/main/java/com/example/dreamhouse/DreamhouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dreamhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamhouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dreamhouse/DreamhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dreamhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
